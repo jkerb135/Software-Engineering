3ca80413f380f55c5fafef349b0accac9dbd110b
Josh Kerbaugh <jkerb135@live.kutztown.edu>
2014-10-22 17:39:48 -0400

    user controller

---
+++ SE/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using SE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SE.Controllers
+{
+    public class UserController : ApiController
+    {
+        public class User
+        {
+            public Guid ApplicationId { get; set; }
+            public Guid UserId { get; set; }
+            public string UserName { get; set; }
+            public bool IsAnonymous { get; set; }
+            public DateTime LastActivityDate { get; set; }
+            public string Password { get; set; }
+        }
+        /// <summary>
+        /// Gets all users in the database
+        /// </summary>
+        /// <returns></returns>
+        WebApiEntites db = new WebApiEntites();
+        public IEnumerable<User> GetAllUsers()
+        {
+            return db.aspnet_Users.Where(x => x.aspnet_Roles.Any(tl => tl.RoleName == "User")).Select(tl => new User { ApplicationId = tl.ApplicationId, UserId = tl.UserId, UserName = tl.UserName, IsAnonymous = tl.IsAnonymous, LastActivityDate = tl.LastActivityDate, Password = tl.aspnet_Membership.Password }).AsEnumerable<User>();
+        }
+        /// <summary>
+        /// Gets user by user id
+        /// </summary>
+        /// <param name="id">System.Guid User ID</param>
+        /// <returns></returns>
+        public IEnumerable<User> GetUserById(Guid id)
+        {
+            return db.aspnet_Users.Where(x => x.UserId == id).Select(tl => new User { ApplicationId = tl.ApplicationId, UserId = tl.UserId, UserName = tl.UserName, IsAnonymous = tl.IsAnonymous, LastActivityDate = tl.LastActivityDate, Password = tl.aspnet_Membership.Password }).AsEnumerable<User>();
+        }
+
+    }
+}
