a7c9388659ec7aa0c68f0b7ee5d0f74ae876f52f
Daniel Talley <danielptalley@gmail.com>
2014-11-12 19:18:21 -0500

    Minor change

--- SE/Scripts/scripts.js
+++ SE/Scripts/scripts.js
@@ -8,19 +8,16 @@
                 var audioExt = [".mp3", ".wav", ".m4a", ".mwa"];
                 var audio = "<audio class='audio-preview' controls>" +
                     "<source src='" + evt.target.result + "'/></audio>";
-                $('.main-step-audio-preview').empty();
                 $('.main-step-audio-preview').append(audio);
             }
             if (mimeType.match(/video/)) {
                 var videoExt = [".mp3", ".wav", ".m4a", ".mwa"];
                 var video = "<video class='video-preview' controls>" +
                     "<source src='" + evt.target.result + "'/></video>";
-                $('.main-step-video-preview').empty();
                 $('.main-step-video-preview').append(video);
             }
             if (mimeType.match(/image/)) {
                 var image = "<img class='image-preview' src='" + evt.target.result + "'/>";
-                $('.detailed-step-image-preview').empty();
                 $('.detailed-step-image-preview').append(image);
             }
         }
@@ -29,13 +26,16 @@
 }
 
 $("body").on('change', '.main-step-audio', function (evt) {
+    $('.main-step-audio-preview').empty();
     showPreview(evt.target.files);
 });
 
 $("body").on('change', '.main-step-video', function (evt) {
+    $('.main-step-video-preview').empty();
     showPreview(evt.target.files);
 });
 
 $("body").on('change', '.detailed-step-image', function (evt) {
+    $('.detailed-step-image-preview').empty();
     showPreview(evt.target.files);
 });
