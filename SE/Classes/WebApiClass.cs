94a78aa0cf5697a724334323756de9410457a563
Josh Kerbaugh <jkerb135@live.kutztown.edu>
2015-04-24 16:00:33 -0400

    Final 4/24/2015

--- SE/Classes/WebApiClass.cs
+++ SE/Classes/WebApiClass.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using SE.Models;
 
 namespace SE.Classes
 {
     public class WebApiClass
     {
-        public class User
+        public new class User
         {
             public Guid ApplicationId { get; set; }
             public Guid UserId { get; set; }
@@ -15,48 +17,54 @@ namespace SE.Classes
             public DateTime LastActivityDate { get; set; }
             public string Password { get; set; }
         }
-
-        public class CategoryClass
-        {
-            public string CategoryName { get; set; }
-            public int CategoryId { get; set; }
-            public List<TaskClass> Tasks { get; set; }
-        }
-        public class TaskClass
+        public class NewTask
         {
             public int TaskId { get; set; }
             public string TaskName { get; set; }
-            public List<MainStepClass> MainSteps { get; set; }
+            public ICollection<TaskAssignment> Assignments { get; set; }
 
         }
 
-        public class CompletedMainStepClass
+        public class NewCategory
         {
-            public int MainStepId { get; set; }
+            public string CategoryName { get; set; }
+            public int CategoryId { get; set; }
+            public List<UserTasks> Tasks { get; set; }
         }
 
-
-        public class MainStepClass
+        public class NewMainStep
         {
-            public int MainStepID { get; set; }
             public string MainStepName { get; set; }
+            public int MainStepId { get; set; }
             public string MainStepText { get; set; }
-            public string AudioPath { get; set; }
             public string VideoPath { get; set; }
-            public int SortOrder { get; set; }
-            public List<CompletedMainStepClass> CompletedMainSteps { get; set; }
-            public List<DetailedStepClass> DetailedSteps { get; set; }
+            public string AudioPath { get; set; }
+            public List<NewDetailedStep> DetailedStep { get; set; }
         }
 
-        public class DetailedStepClass
+        public class NewDetailedStep
         {
-            public int DetailedStepID { get; set; }
+            public int DetailedStepId { get; set; }
             public string DetailedStepName { get; set; }
-            public string DetailedStepText { get; set; }
             public string ImagePath { get; set; }
-            public int SortOrder { get; set; }
+            public string DetailedStepText { get; set; }
         }
 
+        public class UserTasks
+        {
+            public int TaskId { get; set; }
+            public string TaskName { get; set; }
+        }
+
+        public class Task
+        {
+            public int CategoryId { get; set; }
+            public string CategoryName { get; set; }
+            public int TaskId { get; set; }
+            public string TaskName { get; set; }
+            public string AssignedUser { get; set; }
+
+        }
         public class UserRequest
         {
             public string AssignedSupervisor { get; set; }
@@ -71,20 +79,19 @@ namespace SE.Classes
             public string IpAddress { get; set; }
             public bool SignedIn { get; set; }
         }
-        public class CompletedTask
+        public class Completed
         {
-            public int TaskID { get; set; }
-            public string TaskName { get; set; }
+            public int MainStepId { get; set; }
+            public int TaskId { get; set; }
+            public string MainStepName { get; set; }
             public string AssignedUser { get; set; }
             public DateTime DateTimeComplete { get; set; }
             public float TotalTime { get; set; }
-            public float TotalDetailedStepsUsed { get; set; }
         }
         public class CompleteStep
         {
-            public int TaskId { get; set; }
-            public int MainStepId { get; set; }
             public string MainStepName { get; set; }
+            public string AssignedUser { get; set; }
             public DateTime DateTimeComplete { get; set; }
             public double TotalTime { get; set; }
         }
--- SE/Controllers/UserController.cs
+++ SE/Controllers/UserController.cs
@@ -6,25 +6,27 @@ Course			: CSC354 - Software Engineering
 Professor Name	: Dr. Tan
 Assignment # 	: Team B - iPAWS
 Filename		: UserController.cs
-Purpose			: This is the main class file for the WebApiClass that pertains to a specific user. It returns all users, a user by name, a users categories, and a users tasks. This file also
+Purpose			: This is the main class file for the WebAPI that pertains to a specific user. It returns all users, a user by name, a users categories, and a users tasks. This file also
                   handles posting completed mainsteps and competed tasks, users logged in state and ipaddress, and their task requests. Also written is a SaveChanges(DbContext) function that outputs  
                   any problems saving database changes in an inner exception I used this for debugging posting.
 */
-
+using SE.Models;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
-using System.Data.Entity.Validation;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Web.Http;
-using System.Web.Http.Controllers;
 using System.Web.Http.Cors;
 using System.Web.Http.Filters;
+using System.Web.Http.Controllers;
+using System.Data.Entity.Validation;
+using System.Data.Entity;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Web.Http.ModelBinding;
+using Microsoft.Ajax.Utilities;
 using SE.Classes;
-using SE.Models;
 using Category = SE.Models.Category;
 using Task = SE.Models.Task;
 
@@ -63,76 +65,77 @@ namespace SE.Controllers
         public IEnumerable<Category> GetCategoriesByUser(string id)
         {
             return from a in _db.Categories
-                join b in _db.CategoryAssignments on a.CategoryID equals b.CategoryID
-                where b.AssignedUser == id
-                select new Category();
+                   join b in _db.CategoryAssignments on a.CategoryID equals b.CategoryID
+                   where b.AssignedUser == id
+                   select new Category();
         }
 
-        public IList<WebApiClass.CategoryClass> GetByUser(string id)
+        public IList<WebApiClass.NewCategory> GetByUser(string id)
         {
-            return (from a in _db.Categories
-                    join b in _db.CategoryAssignments on a.CategoryID equals b.CategoryID
-                    where b.AssignedUser == id
-                    select new WebApiClass.CategoryClass
-                    {
-                        CategoryId = a.CategoryID,
-                        CategoryName = a.CategoryName,
-                        Tasks = a.Tasks.ToList().Select(t => new WebApiClass.TaskClass
-                        {
-                            TaskId = t.TaskID,
-                            TaskName = t.TaskName,
-                        }).ToList()
-                    }).ToList();
+            List<WebApiClass.NewCategory> list = new List<WebApiClass.NewCategory>();
+            foreach (WebApiClass.NewCategory category in (_db.Categories.Join(_db.CategoryAssignments, a => a.CategoryID, b => b.CategoryID, (a, b) => new { a, b }).Where(@t1 => @t1.b.AssignedUser == id).Select(@t1 => new WebApiClass.NewCategory
+            {
+                CategoryId = @t1.a.CategoryID,
+                CategoryName = @t1.a.CategoryName,
+                Tasks = @t1.a.Tasks.ToList().Select(t => new WebApiClass.UserTasks
+                {
+                    TaskId = t.TaskID,
+                    TaskName = t.TaskName
+                }).ToList()
+            })))
+                list.Add(category);
+            return list;
         }
 
-        public IList<WebApiClass.MainStepClass> GetTaskDetails(int id)
+        public IList<WebApiClass.NewMainStep> GetTaskDetails(int id)
         {
-<<<<<<< HEAD
             List<WebApiClass.NewMainStep> list = new List<WebApiClass.NewMainStep>();
             foreach (WebApiClass.NewMainStep step in (_db.MainSteps.Where(a => a.TaskID == id).Select(a => new WebApiClass.NewMainStep
             {
-                MainStepId = a.MainStepID, MainStepName = a.MainStepName, MainStepText = a.MainStepText, AudioPath = a.AudioPath.Replace("~", "http://ipawsteamb.csweb.kutztown.edu"), VideoPath = a.VideoPath.Replace("~", "http://ipawsteamb.csweb.kutztown.edu"), DetailedStep = a.DetailedSteps.ToList().Select(t => new WebApiClass.NewDetailedStep
-=======
-            return (from a in _db.MainSteps
-                where a.TaskID == id
-                select new WebApiClass.MainStepClass
->>>>>>> origin/master
+                MainStepId = a.MainStepID,
+                MainStepName = a.MainStepName,
+                MainStepText = a.MainStepText,
+                AudioPath = a.AudioPath.Replace("~", "http://ipawsteamb.csweb.kutztown.edu"),
+                VideoPath = a.VideoPath.Replace("~", "http://ipawsteamb.csweb.kutztown.edu"),
+                DetailedStep = a.DetailedSteps.ToList().Select(t => new WebApiClass.NewDetailedStep
                 {
-                    MainStepID = a.MainStepID,
-                    MainStepName = a.MainStepName,
-                    MainStepText = a.MainStepText,
-                    AudioPath = a.AudioPath.Replace("~", "http://ipawsteamb.csweb.kutztown.edu"),
-                    VideoPath = a.VideoPath.Replace("~", "http://ipawsteamb.csweb.kutztown.edu"),
-                    SortOrder = a.ListOrder,
-                    DetailedSteps = a.DetailedSteps.ToList().Select(d => new WebApiClass.DetailedStepClass
+                    DetailedStepId = t.DetailedStepID,
+                    DetailedStepName = t.DetailedStepName,
+                    DetailedStepText = t.DetailedStepText,
+                    ImagePath = t.ImagePath.Replace("~", "http://ipawsteamb.csweb.kutztown.edu"),
+                }).ToList()
+            })))
+                list.Add(step);
+            return list;
+        }
+
+        /// <summary>
+        /// Gets tasks assigned to user by username
+        /// </summary>
+        /// <param name="id">String Username</param>
+        /// <returns></returns>
+        public IEnumerable<WebApiClass.Task> GetTasksByUser(string id)
+        {
+            return
+                _db.Tasks.Join(_db.Categories, task => task.CategoryID, cat => cat.CategoryID,
+                    (task, cat) => new { task, cat })
+                    .Join(_db.TaskAssignments, @t => @t.task.TaskID, assigned => assigned.TaskID,
+                        (@t, assigned) => new { @t, assigned })
+                    .Where(@t => @t.assigned.AssignedUser == id)
+                    .Select(@t => new WebApiClass.Task
                     {
-                        DetailedStepID = d.DetailedStepID,
-                        DetailedStepName = d.DetailedStepName,
-                        DetailedStepText = d.DetailedStepText,
-                        ImagePath = d.ImagePath.Replace("~", "http://ipawsteamb.csweb.kutztown.edu")
-                    }).ToList()
-                }).OrderBy(x => x.SortOrder).ToList();
+                        CategoryId = @t.@t.cat.CategoryID,
+                        CategoryName = @t.@t.cat.CategoryName,
+                        TaskId = @t.@t.task.TaskID,
+                        TaskName = @t.@t.task.TaskName,
+                        AssignedUser = @t.assigned.AssignedUser
+                    });
         }
 
-        public IList<WebApiClass.DetailedStepClass> GetMainStepDetails(int id)
+        public IEnumerable<WebApiClass.CompleteStep> GetAllCompletedSteps()
         {
-            return (from d in _db.DetailedSteps
-                where d.MainStepID == id
-                select new WebApiClass.DetailedStepClass
-                {
-                    DetailedStepID = d.DetailedStepID,
-                    DetailedStepName = d.DetailedStepName,
-                    DetailedStepText = d.DetailedStepText,
-                    ImagePath = d.ImagePath.Replace("~", "http://ipawsteamb.csweb.kutztown.edu"),
-                    SortOrder = d.ListOrder,
-                }).OrderBy(x => x.SortOrder).ToList();
+            return _db.CompletedMainSteps.Select(@t => new WebApiClass.CompleteStep() { AssignedUser = @t.AssignedUser, DateTimeComplete = @t.DateTimeComplete, MainStepName = @t.MainStepName, TotalTime = @t.TotalTime });
         }
-
-        public IEnumerable<WebApiClass.CompleteStep> GetAllCompletedSteps(string id, int val)
-        {
-            return _db.CompletedMainSteps.Where(@u => @u.AssignedUser == id && @u.TaskID == val).Select(@t => new WebApiClass.CompleteStep() { MainStepId = @t.MainStepID, DateTimeComplete = @t.DateTimeComplete, MainStepName = @t.MainStepName, TotalTime = @t.TotalTime }); 
-        }
-
         [HttpPost]
         public HttpResponseMessage PostMainStepCompleted([FromBody]CompletedMainStep mainstep)
         {
@@ -141,25 +144,18 @@ namespace SE.Controllers
                 return Request.CreateResponse(HttpStatusCode.BadRequest);
             }
 
-            var complete =
-                _db.CompletedMainSteps.Where(
-                    x => x.AssignedUser == mainstep.AssignedUser && x.MainStepID == mainstep.MainStepID)
-                    .Select(x => x)
-                    .FirstOrDefault();
+            var complete = _db.CompletedMainSteps.FirstOrDefault(u => u.AssignedUser == mainstep.AssignedUser && u.MainStepID == mainstep.MainStepID && u.MainStepName == mainstep.MainStepName && u.DateTimeComplete == mainstep.DateTimeComplete);
             if (complete != null)
-            {
-                complete.DateTimeComplete = DateTime.Now;
-                complete.TotalTime = mainstep.TotalTime;
-            }
-            else
-            {
-                mainstep.DateTimeComplete = DateTime.Now;
-                _db.CompletedMainSteps.Add(mainstep);
-            }
+                return Request.CreateResponse(HttpStatusCode.Conflict, "Completed Main Step already exists in database");
+            mainstep.DateTimeComplete = DateTime.Now;
+            _db.CompletedMainSteps.Add(mainstep);
             _db.SaveChanges();
             return Request.CreateResponse(HttpStatusCode.OK, "Completed Main Step added to database");
         }
-
+        public IEnumerable<CompletedTask> GetAllCompletedTasks()
+        {
+            return _db.CompletedTasks.AsEnumerable();
+        }
         public HttpResponseMessage PostTaskCompleted([FromBody]CompletedTask task)
         {
             if (!ModelState.IsValid)
@@ -167,45 +163,27 @@ namespace SE.Controllers
                 return Request.CreateResponse(HttpStatusCode.BadRequest);
             }
 
-            var complete =
-                _db.CompletedTasks.Where(
-                    x => x.AssignedUser == task.AssignedUser && x.TaskID == task.TaskID)
-                    .Select(x => x)
-                    .FirstOrDefault();
+            var complete = _db.CompletedTasks.FirstOrDefault(u => u.AssignedUser == task.AssignedUser && u.TaskID == task.TaskID && u.TaskName == task.TaskName && u.DateTimeCompleted == task.DateTimeCompleted);
             if (complete != null)
-            {
-                complete.DateTimeCompleted = DateTime.Now;
-                complete.TotalDetailedStepsUsed = task.TotalDetailedStepsUsed;
-                complete.TotalTime = task.TotalTime;
-            }
-            else
-            {
-                task.DateTimeCompleted = DateTime.Now;
-                _db.CompletedTasks.Add(task);
-            }
-
+                return Request.CreateResponse(HttpStatusCode.Conflict, "Completed Task already exists in database");
+            task.DateTimeCompleted = DateTime.Now;
+            _db.CompletedTasks.Add(task);
             SaveChanges(_db);
             return Request.CreateResponse(HttpStatusCode.OK, "Completed Task added to database");
         }
-
-        public IEnumerable<CompletedTask> GetTasksCompleted(string id)
-        {
-            return _db.CompletedTasks.Where(x => x.AssignedUser == id).Select(x => x);
-        }
-
         public HttpResponseMessage PostLoggedInIp([FromBody]WebApiClass.SendUser user)
         {
             if (!ModelState.IsValid)
             {
                 return Request.CreateResponse(HttpStatusCode.BadRequest);
             }
-                var getUser = _db.MemberAssignments.SingleOrDefault(x => x.AssignedUser == user.Username);
-                if (getUser != null)
-                {
-                    getUser.UsersIp = user.IpAddress;
-                    getUser.IsUserLoggedIn = user.SignedIn;
-                }
-                SaveChanges(_db);
+            var getUser = _db.MemberAssignments.SingleOrDefault(x => x.AssignedUser == user.Username);
+            if (getUser != null)
+            {
+                getUser.UsersIp = user.IpAddress;
+                getUser.IsUserLoggedIn = user.SignedIn;
+            }
+            SaveChanges(_db);
             return Request.CreateResponse(HttpStatusCode.OK, "User Updated");
         }
 
@@ -235,7 +213,6 @@ namespace SE.Controllers
             SaveChanges(_db);
             return Request.CreateResponse(HttpStatusCode.OK, "Task Requested");
         }
-
         private static void SaveChanges(DbContext context)
         {
             try
